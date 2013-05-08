3d1d8fc7aaaa919d49a3f035818a0931681fc81c
Scott Pullen <s.pullen05@gmail.com>
2013-05-08 16:18:05 -0400

    adding some prelim test for parse_fetch_results

--- spec/pubmed/parse_fetch_results_spec.rb
+++ spec/pubmed/parse_fetch_results_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
+module TestModule
+  extend Pubmed::ParseFetchResults
+end
+
 describe Pubmed::ParseFetchResults do
-  
+
+  describe '#parse_medline_cite' do
+    before(:each) do
+      @pubmed_article = Nokogiri::XML(File.open(File.expand_path('spec/fixtures/efetch_rettype_medline.xml'))).xpath("//PubmedArticle[1]").first
+      @expected = {:pubmed_id => '23645694', :date_published => '2013-5-6'}
+    end
+
+    it 'should return a hash containing the pubmed_id and date_published' do
+      TestModule.parse_medline_cite(@pubmed_article.to_xml).should == @expected
+    end
+  end
 end
